810454ebc3b3c089cefff14218fdee81e86e5aef
Sagittarius <singularEngine@gmail.com>
2017-01-18 10:24:02 +0600

    +

--- Assets/Editor/PostProcess.cs
+++ Assets/Editor/PostProcess.cs
@@ -19,7 +19,8 @@ public class PbxModifier
 
             string target = proj.TargetGuidByName("Unity-iPhone"); 
 			proj.SetBuildProperty (target, "ENABLE_BITCODE", "false");
-            string file = proj.FindFileGuidByProjectPath("Libraries/Plugins/iOS/KiiIOSSocialNetworkConnector.mm"); 
+			proj.SetBuildProperty (target, "CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES", "yes");
+           string file = proj.FindFileGuidByProjectPath("Libraries/Plugins/iOS/KiiIOSSocialNetworkConnector.mm"); 
             var flags = proj.GetCompileFlagsForFile(target, file);
             flags.Add("-fno-objc-arc");
 			proj.AddFrameworkToProject (target, "CoreData.framework", false);
