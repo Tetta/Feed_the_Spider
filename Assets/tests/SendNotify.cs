c6c14055e542fc0bce9ac0de5c422d3219e660d4
nicodrive <nicodrive@rambler.ru>
2017-01-23 18:29:00 +0700

    +

--- Assets/tests/SendNotify.cs
+++ Assets/tests/SendNotify.cs
@@ -3,16 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using com.playGenesis.VkUnityPlugin;
 using Facebook.Unity;
+using GoogleMobileAds.Api;
 using UnityEngine;
 using UnityEngine.Advertisements;
 using UnityEngine.UI;
 
 public class SendNotify : MonoBehaviour
-{
-    void Start()
-    {
-        Advertisement.Initialize("1271127");
-    }
+{ 
 
     public Text Text;
     public void Update()
@@ -27,7 +24,7 @@ public class SendNotify : MonoBehaviour
 
     public void ShowAd()
     {
-        Advertisement.Show();
+        RequestInterstitial();
     }
     public void FbLogin()
     {
@@ -38,5 +35,141 @@ public class SendNotify : MonoBehaviour
         });
            
     }
-    
+
+
+
+
+    private BannerView bannerView;
+    private InterstitialAd interstitial;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        RequestBanner();
+        RequestInterstitial();
+        bannerView.Show();
+        if (Application.loadedLevelName == "Game")
+        {
+            bannerView.Hide();
+        }
+    }
+
+    private void RequestBanner()
+    { 
+         string adUnitId = "ca-app-pub-xxxxxxxxxxxxxxxxxxxxxxx"; 
+
+        // Create a 320x50 banner at the top of the screen.
+        bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
+        // Register for ad events.
+        bannerView.OnAdLoaded += HandleAdLoaded;
+        bannerView.OnAdFailedToLoad += HandleAdFailedToLoad;
+        bannerView.OnAdOpening += HandleAdOpened; 
+        bannerView.OnAdClosed += HandleAdClosed;
+        bannerView.OnAdLeavingApplication += HandleAdLeftApplication;
+        // Load a banner ad.
+        bannerView.LoadAd(createAdRequest());
+    }
+
+    private void RequestInterstitial()
+    { 
+         string adUnitId = "INSERT_IOS_INTERSTITIAL_AD_UNIT_ID_HERE"; 
+
+        // Create an interstitial.
+        interstitial = new InterstitialAd(adUnitId);
+        // Register for ad events.
+        interstitial.OnAdLoaded += HandleInterstitialLoaded;
+        interstitial.OnAdFailedToLoad += HandleInterstitialFailedToLoad;
+        interstitial.OnAdOpening+= HandleInterstitialOpened;
+        interstitial.OnAdClosed += HandleInterstitialClosed;
+        interstitial.OnAdLeavingApplication += HandleInterstitialLeftApplication;
+        // Load an interstitial ad.
+        interstitial.LoadAd(createAdRequest());
+    }
+
+    // Returns an ad request with custom ad targeting.
+    private AdRequest createAdRequest()
+    {
+        return new AdRequest.Builder()
+            .AddTestDevice(AdRequest.TestDeviceSimulator)
+                .AddTestDevice("9ACD01B470951161A30C0AA4B6DA3A7D")
+                .AddKeyword("game")
+                .SetGender(Gender.Male)
+                .SetBirthday(new DateTime(1985, 1, 1))
+                .TagForChildDirectedTreatment(false)
+                .AddExtra("color_bg", "9B30FF")
+                .Build();
+
+    }
+
+    private void ShowInterstitial()
+    {
+        if (interstitial.IsLoaded())
+        {
+            interstitial.Show();
+        }
+        else
+        {
+            print("Interstitial is not ready yet.");
+        }
+    }
+     
+    public void HandleAdLoaded(object sender, EventArgs args)
+    {
+        print("HandleAdLoaded event received.");
+    }
+
+    public void HandleAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        print("HandleFailedToReceiveAd event received with message: " + args.Message);
+    }
+
+    public void HandleAdOpened(object sender, EventArgs args)
+    {
+        print("HandleAdOpened event received");
+    }
+
+    void HandleAdClosing(object sender, EventArgs args)
+    {
+        print("HandleAdClosing event received");
+    }
+
+    public void HandleAdClosed(object sender, EventArgs args)
+    {
+        print("HandleAdClosed event received");
+    }
+
+    public void HandleAdLeftApplication(object sender, EventArgs args)
+    {
+        print("HandleAdLeftApplication event received");
+    }
+     
+    public void HandleInterstitialLoaded(object sender, EventArgs args)
+    {
+        print("HandleInterstitialLoaded event received.");
+    }
+
+    public void HandleInterstitialFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        print("HandleInterstitialFailedToLoad event received with message: " + args.Message);
+    }
+
+    public void HandleInterstitialOpened(object sender, EventArgs args)
+    {
+        print("HandleInterstitialOpened event received");
+    }
+
+    void HandleInterstitialClosing(object sender, EventArgs args)
+    {
+        print("HandleInterstitialClosing event received");
+    }
+
+    public void HandleInterstitialClosed(object sender, EventArgs args)
+    {
+        print("HandleInterstitialClosed event received");
+    }
+
+    public void HandleInterstitialLeftApplication(object sender, EventArgs args)
+    {
+        print("HandleInterstitialLeftApplication event received");
+    } 
 }
