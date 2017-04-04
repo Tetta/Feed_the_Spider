108593b994acb27646601400763dfad6540f6917
Sagittarius <singularEngine@gmail.com>
2017-04-04 16:58:57 +0600

    +

--- Assets/Editor/PostProcess.cs
+++ Assets/Editor/PostProcess.cs
@@ -15,28 +15,7 @@ public class PbxModifier
         if (buildTarget == BuildTarget.iOS)
         {
 #if UNITY_IOS
-            string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
-
-            PBXProject proj = new PBXProject();
-            proj.ReadFromString(File.ReadAllText(projPath));
-
-            string target = proj.TargetGuidByName("Unity-iPhone"); 
-
-			proj.SetBuildProperty (target, "CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES", "yes");
-
-			//proj.WriteToFile(projPath);
-
-
-            string plistPath = path + "/Info.plist";
-			var txt = File.ReadAllText(plistPath);
-
-			txt = txt.Replace(
-"<key>LSApplicationQueriesSchemes</key>",
-@"<key>NSPhotoLibraryUsageDescription</key>
-<string>This app requires access to the photo library.</string>
-<key>LSApplicationQueriesSchemes</key>");
-            // Write to file
-            File.WriteAllText(plistPath, txt);
+ 
 
 #endif
         }
